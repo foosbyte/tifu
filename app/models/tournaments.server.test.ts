fbb185a41300d2110e9c1d139f028a5963020f04
ZauberNerd <zaubernerd@zaubernerd.de>
2023-05-16 01:26:26 +0200

    chore: set up testing using node's built-in test runner

---
+++ app/models/tournaments.server.test.ts
@@ -0,0 +1,8 @@
+import * as assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+describe("tournaments", () => {
+  it("should pass", () => {
+    assert.equal(1, 1);
+  });
+});
